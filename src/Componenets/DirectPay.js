2a965439df10b20af9c211124adea49c7b1bd305
xrpgateway <aum.patel.dev@outlook.com>
2023-08-18 00:13:18 +0530

    feat: added split final anim

--- src/Componenets/DirectPay.js
+++ src/Componenets/DirectPay.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 import Convert from "../Animations/convert.json";
-import "./DirectPay.css"
+import "./DirectPay.css";
 import Button from "./Button";
+import signature_animation from "../Animations/signature.json";
+import animationData from "../Animations/done.json";
 
 export default ({ amount }) => {
   const paymentOptions = [
@@ -18,9 +20,10 @@ export default ({ amount }) => {
 
   const [selectedOption, setSelectedOption] = useState(paymentOptions[0]);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isConverting, setConverting] = useState(false)
-  const [cRate, setRate] = useState(1)
-  const [amount_, setAmount] = useState(0)
+  const [isConverting, setConverting] = useState(false);
+  const [cRate, setRate] = useState(1);
+  const [amount_, setAmount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const handleOptionChange = (option) => {
     setSelectedOption(option);
@@ -28,87 +31,135 @@ export default ({ amount }) => {
   };
 
   useEffect(() => {
-    setConverting(true)
-    fetch(`${process.env.REACT_APP_API}/transaction/rate/${selectedOption.text}/${amount}`).then((res) => res.json()).then((res) => {
-      const rate = res.rate
-      const val = res.val
-      setRate(rate)
-      setAmount(val)
-    }).catch(e=> console.error(e)).finally(() => setConverting(false))
-  }, [selectedOption])
+    setConverting(true);
+    fetch(
+      `${process.env.REACT_APP_API}/transaction/rate/${selectedOption.text}/${amount}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const rate = res.rate;
+        const val = res.val;
+        setRate(rate);
+        setAmount(val);
+      })
+      .catch((e) => console.error(e))
+      .finally(() => setConverting(false));
+  }, [selectedOption]);
 
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <div className="flex items-center flex-1" style={{ zIndex: 10 }}>
-        <div className=" text-lg font-semibold mr-2">Pay with</div>
-        <div className="relative inline-block">
-          <div
-            className={`flex items-center cursor-pointer bg-white border border-gray-300 px-4 py-2 rounded-full transition-transform ${
-              dropdownOpen ? "scale-105" : ""
-            }`}
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-          >
-            {selectedOption.icon == "png" ? (
-              <div className="h-full flex justify-center items-center">
-              <img
-                className="mr-2"
-                height={"18px"}
-                width={"18px"}
-                src={selectedOption.url}
-              />
-              </div>
-            ) : (
-              <span className="mr-2">{selectedOption.icon}</span>
-            )}
-            {selectedOption.text}
-          </div>
-          {dropdownOpen && (
+      {step == 1 && (
+        <div className="flex items-center flex-1" style={{ zIndex: 10 }}>
+          <div className=" text-lg font-semibold mr-2">Pay with</div>
+          <div className="relative inline-block">
             <div
-              className="absolute overflow-scroll top-full left-0 mt-2 w-40 bg-white rounded-lg shadow-md overflow-hidden transform origin-top animate-fade-in"
-              style={{ maxHeight: "8rem" }}
-              onAnimationEnd={() => setDropdownOpen(true)}
+              className={`flex items-center cursor-pointer bg-white border border-gray-300 px-4 py-2 rounded-full transition-transform ${
+                dropdownOpen ? "scale-105" : ""
+              }`}
+              onClick={() => setDropdownOpen(!dropdownOpen)}
             >
-              {paymentOptions.map((option) => (
-                <button
-                  key={option.text}
-                  className={`flex items-center justify-start px-4 py-3 w-full ${
-                    selectedOption === option
-                      ? "bg-blue-100 text-blue-500"
-                      : "text-gray-700"
-                  } transition-colors`}
-                  onClick={() => handleOptionChange(option)}
-                >
-                  {option.icon == "png" ? (
-                    <img
-                      className="mr-2"
-                      height={"18px"}
-                      width={"18px"}
-                      src={option.url}
-                    />
-                  ) : (
-                    <span className="mr-2">{option.icon}</span>
-                  )}
-                  {option.text}
-                </button>
-              ))}
+              {selectedOption.icon == "png" ? (
+                <div className="h-full flex justify-center items-center">
+                  <img
+                    className="mr-2"
+                    height={"18px"}
+                    width={"18px"}
+                    src={selectedOption.url}
+                  />
+                </div>
+              ) : (
+                <span className="mr-2">{selectedOption.icon}</span>
+              )}
+              {selectedOption.text}
             </div>
-          )}
+            {dropdownOpen && (
+              <div
+                className="absolute overflow-scroll top-full left-0 mt-2 w-40 bg-white rounded-lg shadow-md overflow-hidden transform origin-top animate-fade-in"
+                style={{ maxHeight: "8rem" }}
+                onAnimationEnd={() => setDropdownOpen(true)}
+              >
+                {paymentOptions.map((option) => (
+                  <button
+                    key={option.text}
+                    className={`flex items-center justify-start px-4 py-3 w-full ${
+                      selectedOption === option
+                        ? "bg-blue-100 text-blue-500"
+                        : "text-gray-700"
+                    } transition-colors`}
+                    onClick={() => handleOptionChange(option)}
+                  >
+                    {option.icon == "png" ? (
+                      <img
+                        className="mr-2"
+                        height={"18px"}
+                        width={"18px"}
+                        src={option.url}
+                      />
+                    ) : (
+                      <span className="mr-2">{option.icon}</span>
+                    )}
+                    {option.text}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      {!isConverting && (<div className="flex items-center mt-5 font-medium">
-        1 XRP ~ {cRate} {selectedOption.text}
-      </div>)}
-      {!isConverting && (<div className="flex items-center mt-1 font-semibold">
-        You will pay ~= {amount_} {selectedOption.text}
-      </div>)}
-      {isConverting && <Lottie
-        style={{ height: 128, width: 128, zIndex: 1 }}
-        animationData={Convert}
-        loop={true}
-      />}
-      <div className="flex-1 flex items-end">
-      <Button text={`Send Payment`} />
-      </div>
+      )}
+      {step == 1 && !isConverting && (
+        <div className="flex items-center mt-5 font-medium">
+          1 XRP ~ {cRate} {selectedOption.text}
+        </div>
+      )}
+      {step == 1 && !isConverting && (
+        <div className="flex items-center mt-1 font-semibold">
+          You will pay ~= {amount_} {selectedOption.text}
+        </div>
+      )}
+      {step == 1 && isConverting && (
+        <Lottie
+          style={{ height: 128, width: 128, zIndex: 1 }}
+          animationData={Convert}
+          loop={true}
+        />
+      )}
+      {step == 1 && (
+        <div className="flex-1 flex items-end">
+          <Button text={`Send Payment`} />
+        </div>
+      )}
+      {step == 2 && (
+        <div className="flex flex-col flex-1 justify-center items-center mt-2">
+          <Lottie
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: "center",
+            }}
+            animationData={signature_animation}
+            loop={true}
+          />
+          <div className="text-lg font-semibold mt-2">
+            Waiting for signature approval!
+          </div>
+        </div>
+      )}
+      {step == 3 && (
+        <div className="flex flex-col flex-1 justify-center items-center mt-2">
+          <Lottie
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: "center",
+            }}
+            animationData={animationData}
+            loop={true}
+          />
+          <div className=" font-semibold mt-3">
+            Transaction Successful 🥳
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Componenets/SplitPay.js
+++ src/Componenets/SplitPay.js
@@ -4,9 +4,13 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import fiveBellsCondition from "five-bells-condition";
 import crypto from "crypto";
+import Lottie from "lottie-react";
+import signature_animation from "../Animations/signature.json";
+import submit_other from "../Animations/submit_other.json";
 
 export default ({ amount }) => {
   const [emails, setEmails] = useState([""]);
+  const [step, setStep] = useState(1);
 
   const addEmailInput = () => {
     setEmails([...emails, ""]);
@@ -39,7 +43,7 @@ export default ({ amount }) => {
         return;
       }
     }
-
+    setStep(2);
     const mySplit = (parseInt(amount) / emails.length).toFixed(0);
     console.log("mySplit", mySplit);
 
@@ -63,7 +67,7 @@ export default ({ amount }) => {
     var payload = {
       txjson: {
         TransactionType: "EscrowCreate",
-        Amount: amount,
+        Amount: mySplit,
         Destination: "rn957ufJRsuErNt4BS8RJTrk9VhJnrBnKc",
         Condition: condition,
         CancelAfter: 3472349872,
@@ -92,25 +96,47 @@ export default ({ amount }) => {
         );
 
         resolved.then(async function (payloadOutcome) {
-          let data = {
-            method: "tx",
-            params: [
-              {
-                transaction: payloadOutcome.txid,
-                binary: false,
-              },
-            ],
-          };
-          let response = await fetch(process.env.REACT_APP_XRPL_API, {
+          const txHash = payloadOutcome.txid;
+          fetch(`${process.env.REACT_APP_API}/transaction/submitted`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify(data),
-          });
+            body: JSON.stringify({
 
-          let responseData = await response.json();
-          console.log(responseData.result);
+            }),
+          })
+            .then((res) => res.json())
+            .then((e) => {
+              if (e.success) {
+                setStep(3);
+                toast.success(
+                  "Congratulations! Your split approved successfully!",
+                  {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                  }
+                );
+              } else {
+                toast.error("Failed to submit transaction!", {
+                  position: "bottom-center",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "dark",
+                });
+                setStep(1);
+              }
+            });
         });
       })
       .catch(function (payloadError) {
@@ -120,48 +146,88 @@ export default ({ amount }) => {
 
   return (
     <div className="p-4 pt-0 h-full">
-      <h4 className="text-lg font-semibold mb-2">Your Email</h4>
-      <input
-        type="email"
-        value={emails[0]}
-        onChange={(e) => handleEmailChange(0, e.target.value)}
-        placeholder="Enter email"
-        className="rounded-full border px-4 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
-      />
-      <h4 className="text-lg font-semibold  mt-3">Invite People</h4>
-      <div
-        className="overflow-scroll px-4 py-1 border border-2 rounded-lg "
-        style={{ maxHeight: "8rem" }}
-      >
-        {emails.slice(1).map((email, index) => (
-          <div key={index} className="flex items-center space-x-2 mb-0.5">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => handleEmailChange(index, e.target.value)}
-              placeholder="Enter email"
-              className="m-1 rounded-full border px-4 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <button
-              onClick={() => removeEmailInput(index)}
-              className="bg-red-500 text-white rounded-full mr-2 px-3 py-1 transition hover:bg-red-600 focus:outline-none"
-            >
-              -
-            </button>
-          </div>
-        ))}
-        <div>
-          <button
-            onClick={addEmailInput}
-            className="bg-green-500 text-white rounded-full px-3 py-1  ml-1 transition hover:bg-green-600 focus:outline-none"
+      {step == 1 && (
+        <>
+          <h4 className="text-lg font-semibold mb-2">Your Email</h4>
+          <input
+            type="email"
+            value={emails[0]}
+            onChange={(e) => handleEmailChange(0, e.target.value)}
+            placeholder="Enter email"
+            className="rounded-full border px-4 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
+          />
+          <h4 className="text-lg font-semibold  mt-3">Invite People</h4>
+          <div
+            className="overflow-scroll px-4 py-1 border border-2 rounded-lg "
+            style={{ maxHeight: "8rem" }}
           >
-            +
-          </button>
+            {emails.slice(1).map((email, index) => (
+              <div
+                key={index + 1}
+                className="flex items-center space-x-2 mb-0.5"
+              >
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => handleEmailChange(index + 1, e.target.value)}
+                  placeholder="Enter email"
+                  className="m-1 rounded-full border px-4 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
+                />
+                <button
+                  onClick={() => removeEmailInput(index + 1)}
+                  className="bg-red-500 text-white rounded-full mr-2 px-3 py-1 transition hover:bg-red-600 focus:outline-none"
+                >
+                  -
+                </button>
+              </div>
+            ))}
+            <div>
+              <button
+                onClick={addEmailInput}
+                className="bg-green-500 text-white rounded-full px-3 py-1  ml-1 transition hover:bg-green-600 focus:outline-none"
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <Button onClick={onPaymentInit} text={"Initiate Split Pyament"} />
+          </div>
+        </>
+      )}
+      {step == 2 && (
+        <div className="flex flex-col flex-1 justify-center items-center mt-2">
+          <Lottie
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: "center",
+            }}
+            animationData={signature_animation}
+            loop={true}
+          />
+          <div className="text-lg font-semibold mt-2">
+            Waiting for signature approval!
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center mt-2">
-        <Button onClick={onPaymentInit} text={"Initiate Split Pyament"} />
-      </div>
+      )}
+      {step == 3 && (
+        <div className="flex flex-col flex-1 justify-center items-center mt-2">
+          <Lottie
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: "center",
+            }}
+            animationData={submit_other}
+            loop={true}
+          />
+          <div className="text-center font-semibold mt-2">
+            Your transaction will be successful once all invited parties approve
+            it.
+          </div>
+        </div>
+      )}
       <ToastContainer />
     </div>
   );
